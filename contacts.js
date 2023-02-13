2f7371e187946233ce994dc99fcfcff59d9503ff
Your NameSoan1488 <andrisokolovskiy@gmail.com>
2023-02-13 22:57:17 +0100

    did hw-1

---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require("fs").promises;
+const path = require("path");
+const shortid = require("shortid");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find((elem) => contactId === Number(elem.id));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const filteredContacts = contacts.filter(
+      (elem) => Number(elem.id) !== contactId
+    );
+    fs.writeFile(contactsPath, JSON.stringify(filteredContacts), "utf-8");
+  } catch (error) {}
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    contacts.push({ id: shortid.generate(), name, email, phone });
+    fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
